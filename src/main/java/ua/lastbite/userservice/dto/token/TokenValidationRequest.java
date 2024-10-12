47d31d3db257edb3198e8762fca4f61208010912
Augustin Gerasym <augustingerasym@gmail.com>
2024-10-12 22:19:45 +0200

    Created TokenValidationRequest.java

---
+++ src/main/java/ua/lastbite/userservice/dto/token/TokenValidationRequest.java
@@ -0,0 +1,15 @@
+package ua.lastbite.userservice.dto.token;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class TokenValidationRequest {
+
+    @NotBlank(message = "Token cannot be empty")
+    private String tokenValue;
+}
