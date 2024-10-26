7c8dd1f2854fac2a50cb6e30622e7e0f1c973038
Augustin Gerasym <augustingerasym@gmail.com>
2024-10-26 18:55:40 +0200

    Added integration tests in UserEmailControllerIntegrationTest.java

--- src/test/java/ua/lastbite/userservice/controller/UserEmailControllerIntegrationTest.java
+++ src/test/java/ua/lastbite/userservice/controller/UserEmailControllerIntegrationTest.java
@@ -1,18 +1,25 @@
 package ua.lastbite.userservice.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import ua.lastbite.userservice.dto.token.TokenValidationRequest;
+import ua.lastbite.userservice.dto.token.TokenValidationResponse;
 import ua.lastbite.userservice.model.CountryCode;
 import ua.lastbite.userservice.model.User;
 import ua.lastbite.userservice.model.UserRole;
 import ua.lastbite.userservice.repository.UserRepository;
+import ua.lastbite.userservice.service.TokenServiceClient;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -29,9 +36,17 @@ public class UserEmailControllerIntegrationTest {
     private MockMvc mockMvc;
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    TokenServiceClient tokenServiceClient;
+
+    @Autowired
     JdbcTemplate jdbcTemplate;
 
     private User user;
+    private TokenValidationRequest tokenValidationRequest;
+    private TokenValidationResponse tokenValidationResponse;
 
     @BeforeEach
     void cleanUpDatabase() {
@@ -48,6 +63,10 @@ public class UserEmailControllerIntegrationTest {
         user.setPhoneNumber("123456789");
         user.setCountryCode(CountryCode.UA);
         user.setRole(UserRole.CUSTOMER);
+
+        tokenValidationRequest = new TokenValidationRequest("tokenValue123");
+        tokenValidationResponse = new TokenValidationResponse(true, 1);
+
     }
 
     @Test
@@ -65,5 +84,56 @@ public class UserEmailControllerIntegrationTest {
         mockMvc.perform(get("/api/email/1/info"))
                 .andExpect(status().isNotFound())
                 .andExpect(content().string("User with ID 1 not found"));
+    }
+
+    @Test
+    void testVerifyEmailSuccessfully() throws Exception {
+        userRepository.save(user);
+
+        Mockito.when(tokenServiceClient.verifyToken(tokenValidationRequest))
+                .thenReturn(tokenValidationResponse);
+
+        mockMvc.perform(post("/api/email/verify-email")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(tokenValidationRequest)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testVerifyEmailUserNotFound() throws Exception {
+
+        Mockito.when(tokenServiceClient.verifyToken(tokenValidationRequest))
+                .thenReturn(tokenValidationResponse);
+
+        mockMvc.perform(post("/api/email/verify-email")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(tokenValidationRequest)))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("User with ID 1 not found"));
+    }
+
+    @Test
+    void testVerifyEmailWithNullToken() throws Exception {
+        tokenValidationRequest.setTokenValue(null);
+
+        Mockito.when(tokenServiceClient.verifyToken(tokenValidationRequest))
+                .thenReturn(tokenValidationResponse);
+
+        mockMvc.perform(post("/api/email/verify-email")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(tokenValidationRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.tokenValue").value("Token cannot be empty"));
+    }
+
+    @Test
+    void testVerifyEmailRequestIsNull() throws Exception {
+        tokenValidationRequest = null;
+
+        mockMvc.perform(post("/api/email/verify-email")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(tokenValidationRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("Request body is missing or invalid"));
     }
 }
