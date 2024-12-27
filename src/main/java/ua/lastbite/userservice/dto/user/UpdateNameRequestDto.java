0eb20226d53237fec46ca9cb199ca872601fa54c
Augustin Gerasym <augustingerasym@gmail.com>
2024-12-27 01:19:06 +0100

    Removed NotBlank validation and added custom ValidSize annotation to UpdateNameRequestDto

--- src/main/java/ua/lastbite/userservice/dto/user/UpdateNameRequestDto.java
+++ src/main/java/ua/lastbite/userservice/dto/user/UpdateNameRequestDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import ua.lastbite.userservice.validation.ValidName;
+import ua.lastbite.userservice.validation.ValidSize;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,13 +13,11 @@ import ua.lastbite.userservice.validation.ValidName;
 @EqualsAndHashCode
 public class UpdateNameRequestDto {
 
-    @NotBlank(message = "First name cannot be empty")
     @ValidName
-    @Size(min = 2, max = 100, message = "First name must be between 2 and 100 characters")
+    @ValidSize(min = 2, max = 100, message = "First name must be between 2 and 100 characters")
     private String firstName;
 
-    @NotBlank(message = "Last name cannot be empty")
     @ValidName
-    @Size(min = 2, max = 100, message = "Last name must be between 2 and 100 characters")
+    @ValidSize(min = 2, max = 100, message = "Last name must be between 2 and 100 characters")
     private String lastName;
 }
