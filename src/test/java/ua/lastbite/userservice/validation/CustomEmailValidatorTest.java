40fdc33a1cba628ff67f289eeada7116b74fc8b0
Augustin Gerasym <augustingerasym@gmail.com>
2025-01-24 20:49:34 +0100

    Renamed EmailValidator to CustomEmailValidator.

--- src/test/java/ua/lastbite/userservice/validation/CustomEmailValidatorTest.java
+++ src/test/java/ua/lastbite/userservice/validation/CustomEmailValidatorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
-class EmailValidatorTest {
+class CustomEmailValidatorTest {
 
     private CustomEmailValidator emailValidator;
     private ConstraintValidatorContext context;
